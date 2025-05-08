595b154c5db5638b150cfc1d087214b6427a714c
Vina del Mar <vina@duoc.acad>
2025-05-07 22:20:44 -0400

    ADD: Agregando y relacionando otra entidad

---
+++ hospital_vm/src/main/java/com/hospital_vm/cl/hospital_vm/model/Atencion.java
@@ -0,0 +1,31 @@
+package com.hospital_vm.cl.hospital_vm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "atencion")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Atencion {
+
+    @ManyToOne
+    private Paciente paciente;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Date fechaAtencion;
+
+    @Column(nullable = false)
+    private int costo;
+
+
+}
