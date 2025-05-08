456eb3b694294f27c799ca6c874de8c9226cca5c
Vina del Mar <vina@duoc.acad>
2025-05-08 19:56:57 -0400

    ADD: atencion repository

---
+++ hospital_vm/src/main/java/com/hospital_vm/cl/hospital_vm/repository/AtencionRepository.java
@@ -0,0 +1,11 @@
+package com.hospital_vm.cl.hospital_vm.repository;
+
+import com.hospital_vm.cl.hospital_vm.model.Atencion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AtencionRepository extends JpaRepository<Atencion, Integer> {
+}
